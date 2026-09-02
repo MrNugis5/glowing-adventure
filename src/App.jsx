49ff77aae03f3fb32c5be52e7e6a53eca47b83ba
MrNugis5 <martin.nugis@voco.ee>
2026-09-02 08:47:00 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+import ExpenseItem from "./components/ExpenseItem";
 
+
+const App = () => {
+    return (
+    <ExpenseItem expenseData={{
+  title: "Groceries",
+  price: 42,
+  date: new Date(2026, 8, 2)
+}} />
+
+    );
+};
 export default App;
