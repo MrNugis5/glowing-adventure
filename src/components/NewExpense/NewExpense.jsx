05942181547ac7f8b6190f0906a21d57461ee533
MrNugis5 <martin.nugis@voco.ee>
2026-09-21 08:11:48 +0000

    ylesanne 6

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,21 +1,46 @@
+import { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [editForm, setEditForm] = useState(false);
+
+    const startEditingHandler = () => {
+        setEditForm(true);
+    };
+
+    const stopEditingHandler = () => {
+        setEditForm(false);
+    };
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
+
         props.onAddExpense(expenseData);
-    }
-    
-    return(
+
+        // Pärast andmete lisamist sulgeme vormi
+        setEditForm(false);
+    };
+
+    return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!editForm && (
+                <button onClick={startEditingHandler}>
+                    Add New Expense
+                </button>
+            )}
+
+            {editForm && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default NewExpense;
-    
